51459597039b1decb0750c2d886ef22d3e34fa35
Saragon4005 <raven.keszei@gmail.com>
2026-02-13 14:35:46 -0800

    write out skeleton

--- Assigment_1/countnames.c
+++ Assigment_1/countnames.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
-#define NAME_LENGTH 30
-#define NUM_NAMES 101
+#define NAME_LENGTH 31  // 30 Chars + '\0'
+#define NUM_NAMES 101   // 100 names + ' '
 
-int main() {
+int main(int argc, char* argv[]) {
   int counts[NUM_NAMES];
   char names[NUM_NAMES][NAME_LENGTH];
+
+  // determine if file name is given or stdin should be read directly (argc == 2
+  // or 1)
+  if (argc == 1) {
+    /* code */
+  }
+
+  // loop through lines (how does the last line work?)
+  // discard line if empty (emmit warning)
+  // loop through names comparing with the one read in
+  // some mechanism to check if its not been seen (check for default? or
+  // counter)
 
   printf("Hello, World!");
   return 0;
