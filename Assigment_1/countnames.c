cf90293081933b5e7fbfd5434c249b8f66900141
Saragon4005 <raven.keszei@gmail.com>
2026-02-13 13:45:51 -0800

    initial commit

---
+++ Assigment_1/countnames.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define NAME_LENGHT 30
+#define NUM_NAMES 101
+
+int main()
+{
+    int counts[NUM_NAMES];
+    char names[NUM_NAMES][NAME_LENGHT];
+
+    printf("Hello, World!");
+    return 0;
+}
