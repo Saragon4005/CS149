bb8ee17d012bfbc864541cc854cf7e67c724e6d1
Saragon4005 <raven.keszei@gmail.com>
2026-02-17 11:50:14 -0800

    deal with 30 char names

--- Assigment_1/countnames.c
+++ Assigment_1/countnames.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NAME_LENGTH 31  // 30 Chars + '\0'
-#define NUM_NAMES 101   // 100 names + ' '
+#define NAME_LENGTH 31 // 30 Chars + '\0'
+#define NUM_NAMES 101  // 100 names + ' '
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   int counts[NUM_NAMES] = {0};
   char names[NUM_NAMES][NAME_LENGTH];
 
   // determine if file name is given or stdin should be read directly (argc == 2
   // or 1)
 
-  FILE* fp;
+  FILE *fp;
 
   if (argc == 1) {
     fp = stdin;
@@ -29,9 +29,9 @@ int main(int argc, char* argv[]) {
 
   // loop through lines (how does the last line work?)
 
-  char buffer[NAME_LENGTH];
+  char buffer[NAME_LENGTH + 1]; // max size of a name plus 1 for \n
 
-  for (int i = 1; fgets(buffer, NAME_LENGTH, fp) != NULL; i++) {
+  for (int i = 1; fgets(buffer, sizeof(buffer), fp) != NULL; i++) {
     buffer[strcspn(buffer, "\n")] = '\0';
     // remove trailing newline character
     // discard line if empty (emmit warning)
@@ -41,8 +41,8 @@ int main(int argc, char* argv[]) {
     }
     // printf("Line %d: %s\n", i, buffer);
     // loop through names comparing with the one read in
-    for (int j = 0; j < NUM_NAMES; j++) {  // break out of code internally
-      if (counts[j] == 0) {                // name not seen before
+    for (int j = 0; j < NUM_NAMES; j++) { // break out of code internally
+      if (counts[j] == 0) {               // name not seen before
         strcpy(names[j], buffer);
         counts[j] = 1;
         break;
